949607728fe835fc19efaa75464441d6901acb32
thomascords <120433691+thomascords@users.noreply.github.com>
2022-12-12 21:59:34 -0500

    Update thomasfinalproject.py
    added a class, and a list for the user's product, as well as appending it to that list

--- thomasfinalproject.py
+++ thomasfinalproject.py
@@ -1,4 +1,19 @@
+class Product:
+    def __init__(self, name, productID, brand, price):
+        self.name = name
+        self.productID = productID
+        self.brand = brand
+        self.price = price
+
 name = input("Enter product name: ")#asking the user for their information
-product_id = input("Enter product ID: ")
+productID = input("Enter product ID: ")
 brand = input("Enter product brand: ")
 price = input(":Enter product price: ")
+
+productInfo = []#setting up the list for the product information
+customProduct = Product(name, productID, brand, price)
+productInfo.append(customProduct.name)#appending the information from the class to the list
+productInfo.append(customProduct.productID)
+productInfo.append(customProduct.brand)
+productInfo.append(customProduct.price)
+print(productInfo)#testing if it works
