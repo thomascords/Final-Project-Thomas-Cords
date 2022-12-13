7877bd98c18f9dca2a94d3a818a9c0c5b6b3ceb9
thomascords <120433691+thomascords@users.noreply.github.com>
2022-12-12 22:21:57 -0500

    Update thomasfinalproject.py
    adding 2 loops and a try exception in case of errors

--- thomasfinalproject.py
+++ thomasfinalproject.py
@@ -4,21 +4,33 @@ class Product:
         self.productID = productID
         self.brand = brand
         self.price = price
-
-name = input("Enter product name: ")#asking the user for their information
-productID = input("Enter product ID: ")
-brand = input("Enter product brand: ")
-price = input(":Enter product price: ")
-
-productInfo = []#setting up the list for the product information
-customProduct = Product(name, productID, brand, price)
-productInfo.append(customProduct.name)#appending the information from the class to the list
-productInfo.append(customProduct.productID)
-productInfo.append(customProduct.brand)
-productInfo.append(customProduct.price)
-print(productInfo)#testing if it works
-
 try:
     file = open("FinalExam.txt","x")#creating the file if it does not exist
 except:
     file = open("FinalExam.txt", "w")#opening the file if it already exists
+loop = 0
+totalRuns = 0
+while loop == 0:
+    try:
+        totalRuns = int(input("Please enter the amount of products you would like to enter: "))
+        loop = 1
+    except:
+        print("That wasn't a valid answer")
+while totalRuns > 0:
+
+    name = input("Enter product name: ")#asking the user for their information
+    productID = input("Enter product ID: ")
+    brand = input("Enter product brand: ")
+    price = input(":Enter product price: ")
+
+    productInfo = []
+    customProduct = Product(name, productID, brand, price)
+    productInfo.append(customProduct.name)#appending the information from the class to the list
+    productInfo.append(customProduct.productID)
+    productInfo.append(customProduct.brand)
+    productInfo.append(customProduct.price)
+    print(productInfo)
+    totalRuns = totalRuns -1
+    print(totalRuns)
+
+
