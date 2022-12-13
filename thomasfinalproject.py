9f41c03c3f03d70b2e68aa7b31d75034d5274386
thomascords <120433691+thomascords@users.noreply.github.com>
2022-12-12 21:46:56 -0500

    Update thomasfinalproject.py
    collecting inputs

--- thomasfinalproject.py
+++ thomasfinalproject.py
@@ -1,1 +1,4 @@
-print("Test commit")
+name = input("Enter product name: ")#asking the user for their information
+product_id = input("Enter product ID: ")
+brand = input("Enter product brand: ")
+price = input(":Enter product price: ")
